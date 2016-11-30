b3e8af8da1d7745852906d40ffed2badeab73acf
Carlos Alberto Mata Gil <CeuxDruman@gmail.com>
2016-11-30 16:40:45 +0100

    Proyecto Maven con 1 dependencia online + 1 local
    Proyecto Java creado con Maven a partir del Maven archetype: "mvn archetype:generate -DarchetypeArtifactId=maven-archetype-quickstart", con dependencia online (junit 4.12) y una dependencia local (jgrapht 1.0.0).

---
+++ src/test/java/org/egc/AppTest.java
@@ -0,0 +1,26 @@
+package org.egc;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import org.jgrapht.*;
+import org.jgrapht.graph.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    /**
+     * Rigurous Test :-)
+     */
+    @Test
+    public void app()
+    {
+        DirectedGraph<String, DefaultEdge> graph = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+        assertNotNull( graph );
+        assertTrue( true );
+    }
+
+}
